2b49e5e47a39b99386644c5a215a5498849f6e3d
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2025-06-04 01:03:10 -0700

    update issued splot to prev stallf

--- ucsbece154b_top_tb.v
+++ ucsbece154b_top_tb.v
@@ -29,7 +29,7 @@ wire [31:0] InstrD = top.riscv.dp.InstrD;
 wire [31:0] InstrD2 = top.riscv.dp.InstrD2;
 
 wire Hazard = top.riscv.dp.Hazard;
-wire issuedSlot2ThisCycle = top.riscv.dp.issuedSlot2ThisCycle;
+wire StallF2_i_prev = top.riscv.dp.StallF2_i_prev;
 
 integer cycle_count;
 integer instruction_count;
