0942937e6108766768d76c86c63bba131857e1b2
Justin Wu <justin_wu@ucsb.edu>
2025-06-06 15:32:36 -0700

    try revert

--- ucsbece154b_controller.v
+++ ucsbece154b_controller.v
@@ -487,13 +487,12 @@ always @(posedge clk) begin
                 ((Rs1D_i  == RdE2_i && RdE2_i != 0) ||
                  (Rs2D_i  == RdE2_i && RdE2_i != 0));
 
-   assign Hazard = RAW || WAW || loadUse1 || loadUse2; 
-   assign BranchJump = BranchD || JumpD || BranchD2 || JumpD2;
+   assign Hazard = RAW || WAW || loadUse1 || loadUse2 || BranchD || JumpD || BranchD2 || JumpD2;
 
    assign StallF2_o = Hazard;
    assign StallD2_o = Hazard;
-   assign FlushD2_o = Hazard || BranchJump;
-   assign FlushE2_o = Hazard || BranchJump;
+   assign FlushD2_o = Hazard || Mispredict_i;
+   assign FlushE2_o = Hazard || Mispredict_i;
 
 
 endmodule
