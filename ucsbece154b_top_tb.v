12dc3802681ce0e396712befbd75e3255968f1ee
Justin Wu <justin_wu@ucsb.edu>
2025-06-06 16:28:10 -0700

    try update tb to stopcorrectly

--- ucsbece154b_top_tb.v
+++ ucsbece154b_top_tb.v
@@ -76,9 +76,8 @@ initial begin
     @(posedge clk);
     reset = 0;
 
-    for (i = 0; i < 500/** &&
-         !((prev_pc1 == top.riscv.dp.PCF_o && top.riscv.dp.InstrF_i == 32'h00000013) &&
-           (prev_pc2 == top.riscv.dp.PCF2_o && top.riscv.dp.InstrF2_i == 32'h00000013))*/; i = i + 1) begin
+    for (i = 0; i < 500 &&
+         !((prev_pc1 == top.riscv.dp.PCF_o && top.riscv.dp.PCE == 32'h0001005c)); i = i + 1) begin
 
         @(posedge clk);
         prev_pc1 <= top.riscv.dp.PCF_o;
